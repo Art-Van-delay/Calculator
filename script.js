382b40354fa9f4e78928849a0fd1a5c4057279a0
Art <mnsenna@protonmail.com>
2023-02-13 18:12:43 +1000

    Complete button setup

---
+++ script.js
@@ -0,0 +1,29 @@
+// functions
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, a, b) {
+    if (operator === '+') {
+        add(a, b);
+    } else if (operator === '-') {
+        subtract(a, b);
+    } else if (operator === '*') {
+        multiply(a, b);
+    } else {
+        divide(a, b);
+    }
+}
