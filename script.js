ae8f5cfc3822240da9c94c7c1903e1723b7a2c2a
Art <mnsenna@protonmail.com>
2023-03-12 16:07:45 +1000

    Implement delete functionality

--- script.js
+++ script.js
@@ -51,13 +51,14 @@ let space = ' ';
 
 
 const inputScreen = document.querySelector('.input-screen');
-const evalScreen = document.querySelector('.eval-screen');
+
 
 
 const buttons = document.querySelectorAll('button');
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
+        let problemArray = [];
         if (button.className === 'digit') {
             console.log(button.id);
             value += button.id;
@@ -68,15 +69,23 @@ buttons.forEach((button) => {
             inputScreen.textContent = value;
         } else if (button.id === '=') {
             // evaulatedValue = Parser.evaluate(value);
-            let problemArray = [];
+
             problemArray = value.split(" ");
             console.log(problemArray);
             console.log(operateOnArray(problemArray));
-            console.log(button.id);
+            //evalScreen.textContent = operateOnArray(problemArray);
+            inputScreen.textContent = operateOnArray(problemArray);
             value = '';
-            inputScreen.textContent = `${value}`;
             problemArray = [];
-            evalScreen.textContent = operateOnArray(problemArray);
+        } else if (button.id === 'clear') {
+            value = '';
+            problemArray = [];
+            inputScreen.textContent = value;
+        } else if (button.id === 'delete') {
+            // let deletedValueArray = problemArray.pop();
+            // console.log(`${deletedValueArray}`);
+            // inputScreen.textContent = deletedValueArray;
+            inputScreen.textContent = inputScreen.textContent.toString().slice(0, -1);
         }
     });
 });
